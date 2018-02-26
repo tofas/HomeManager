98fac04bb2aa6ff162637588e9d5c586b7b710d1
Daniel Morales <dani.mocab@gmail.com>
2018-02-26 20:05:00 +0100

    fixes

--- app/src/main/java/com/homemanager/view/SplashView.kt
+++ app/src/main/java/com/homemanager/view/SplashView.kt
@@ -1,12 +1,14 @@
 package com.homemanager.view
 
+import com.homemanager.R
 import com.homemanager.base.BaseView
+import com.homemanager.navigator.SplashNavigator
 import com.homemanager.presenter.SplashPresenter
 
 
-class SplashView : BaseView<SplashPresenter>() {
+class SplashView : BaseView<SplashPresenter>(), SplashPresenter.View {
   override fun setContentLayout(): Int {
-
+    return R.layout.empty_activity
   }
 
   override fun initPresenter(): SplashPresenter {
